e30952cd900bd94ba0ccf67e9f9ff4de70f62730
onolox <onolox@gmail.com>
2022-10-15 18:05:09 -0300

    .

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('"smartsemicolon" is now active!');
 	let posicaoOriginal: vscode.Position;
 	let inserido: boolean = false;
 
