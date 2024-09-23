39ba0921856687f9d940c1c25046ed906b21a76c
Fabio <onolox2@gmail.com>
2024-09-23 10:39:38 -0300

    Added rust. Corrected an error in package.json.

--- out/extension.js
+++ out/extension.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = void 0;
 const vscode = require("vscode");
 function activate(context) {
-    console.log('"smartsemicolon" is now active!');
     let posicaoOriginal;
     let inserido = false;
     let disposable = vscode.commands.registerCommand('extension.smartSemicolon', () => {
